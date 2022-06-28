79cba4c3a0fdd04a9a757b8f3adbd00136860b79
Elvis Yung <elvisckyung@gmail.com>
2022-06-28 17:04:29 +0800

    Add files via upload

---
+++ Data Cleaning/longest.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/usr/bin/env python
+from __future__ import print_function
+from Bio import SeqIO
+from argparse import ArgumentParser
+
+
+def main():
+    args = get_args()
+    last_gene = None
+    transcripts = []
+
+    for record in SeqIO.parse(args.input, "fasta"):
+        gene = record.id.split("_")[0]
+
+        if last_gene is None:
+            last_gene = gene
+
+        if last_gene == gene:
+            transcripts.append(record)
+        else:
+            print_longest_transcripts(transcripts, args.number)
+            last_gene = gene
+            transcripts = [record]
+
+    print_longest_transcripts(transcripts, args.number)
+
+
+def print_longest_transcripts(transcripts, number):
+    longest = sorted(transcripts, key=lambda x: len(x.seq), reverse=True)
+
+    for record in longest[:number]:
+        print(">"+record.id, record.seq, sep="\n")
+
+
+# In[ ]:
+
+
+
+
